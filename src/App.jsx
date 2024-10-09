89af2463bf143abd04a6986978a6b2a07d5b04d7
naol <naolmeseret22@gmail.com>
2024-10-09 12:51:39 +0300

    i finished about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Home from './pages/Home'
 import { TracingBeam } from './components/ui/tracing-beam'
 import About from './pages/About'
+import Skill from './pages/Skill'
 export default function App() {
   return (
-    <div  >
+    <>
       <TracingBeam>
       <Home />
       <About />
+      <Skill />
       </TracingBeam>
-    </div>
+    </>
   )
 }
